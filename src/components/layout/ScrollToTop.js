073985dc69b1a895606992828ff577954630a1fd
chetansirohi <35464633+chetansirohi@users.noreply.github.com>
2023-06-12 18:22:23 +0530

    refactored app

--- src/components/layout/ScrollToTop.js
+++ src/components/layout/ScrollToTop.js
@@ -2,17 +2,17 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const ScrollToTop = () => {
-    const { pathname } = useLocation();
+  const { pathname } = useLocation();
 
-    useEffect(() => {
-        document.documentElement.scrollTo({
-            top: 0,
-            left: 0,
-            behavior: 'instant',
-        });
-    }, [pathname]);
+  useEffect(() => {
+    document.documentElement.scrollTo({ 
+      top: 0, 
+      left: 0, 
+      behavior: 'instant', 
+    });
+  }, [pathname]);
 
-    return null;
+  return null;
 };
 
 export default ScrollToTop;
--- src/components/pages/Home/Hero.js
+++ src/components/pages/Home/Hero.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import restaurantFoodImage from './assets/restaurant-food.jpg';
+import restaurantFoodImage from './assets/main-img.jpg';
 import './Hero.css';
 import pages from '../../../utils/pages';
 
@@ -11,17 +11,17 @@ const Hero = () => {
           <h1>Little Lemon</h1>
           <h2>Chicago</h2>
           <p>
-            We are a family owned Mediterranean restaurant, 
+            We are a family owned Mediterranean restaurant,
             focused on traditional recipes served with a modern twist.
           </p>
           <Link className="button-primary" to={pages.get('bookings').path}>
             Reserve a table
           </Link>
         </div>
-        <img 
-          className="hero-image" 
-          src={restaurantFoodImage} 
-          alt="Restaurant food" 
+        <img
+          className="hero-image"
+          src={restaurantFoodImage}
+          alt="Restaurant food"
         />
       </div>
     </section>
